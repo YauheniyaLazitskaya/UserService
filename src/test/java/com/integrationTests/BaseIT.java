1f637ef81067877d3393971325d2cd9c06f8fb11
YauheniyaLazitskaya <yauheniya.lazitskaya@innowise.com>
2025-11-14 22:08:04 +0300

    use singleton

--- src/test/java/com/integrationTests/BaseIT.java
+++ src/test/java/com/integrationTests/BaseIT.java
@@ -1,23 +1,40 @@
 package com.integrationTests;
 
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Testcontainers
 @ActiveProfiles("test")
+@ContextConfiguration(initializers = BaseIT.ContainersInitializer.class)
 public abstract class BaseIT {
-    @Container
-    @ServiceConnection
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15-alpine");
-    @Container
-    @ServiceConnection
-    static GenericContainer<?> redis = new GenericContainer<>(DockerImageName.parse("redis:7-alpine"))
-            .withExposedPorts(6379);
+
+    static class ContainersInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+        static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15-alpine");
+        static GenericContainer<?> redis = new GenericContainer<>(DockerImageName.parse("redis:7-alpine"))
+                .withExposedPorts(6379);
+
+        static {
+            postgres.start();
+            redis.start();
+        }
+
+        @Override
+        public void initialize(ConfigurableApplicationContext applicationContext) {
+            TestPropertyValues.of(
+                    "spring.datasource.url=" + postgres.getJdbcUrl(),
+                    "spring.datasource.username=" + postgres.getUsername(),
+                    "spring.datasource.password=" + postgres.getPassword(),
+                    "spring.data.redis.host=" + redis.getHost(),
+                    "spring.data.redis.port=" + redis.getMappedPort(6379).toString()
+            ).applyTo(applicationContext.getEnvironment());
+        }
+    }
 }
